bd754c9262c4c674266f88de2b5b07c9efe0997e
Ricardo Huitron <ricardoharaiz@gmail.com>
2025-04-13 15:46:00 -0600

    Fix: uso config sin vitePreprocess para Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: vercel()
-  },
-  preprocess: vitePreprocess()
+  }
 };
 
 export default config;
